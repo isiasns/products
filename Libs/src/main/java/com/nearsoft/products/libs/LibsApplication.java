7e5efcea05da02e05ceb16ad0b781ed6897f107e
Isaac Sias <isias@nearsoft.com>
2018-07-09 10:19:22 -0700

    Libs #3 Se agrega la estructura inicial del proyecto de librerias comunes.

---
+++ Libs/src/main/java/com/nearsoft/products/libs/LibsApplication.java
@@ -0,0 +1,12 @@
+package com.nearsoft.products.libs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibsApplication.class, args);
+    }
+}
